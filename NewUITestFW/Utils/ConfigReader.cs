8aa6fa5b7a2e5d1f27877c602c0124cbc7236416
vanch <vanch@DESKTOP-0ENK0U8>
2016-12-21 08:22:23 +0200

    'aaa'

---
+++ NewUITestFW/Utils/ConfigReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewUITestFW.Utils
+{
+    class ConfigReader
+    {
+        public string InitializeTest()
+        {
+            return ConfigurationManager.AppSettings["appUrl"].ToString();
+        }
+    }
+}
