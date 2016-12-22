f2d8329a3738a21c4dff05d938714921132cc5a6
vancho815 <ruslan.sadyhov.contractor@weather.com>
2016-12-22 16:56:31 +0200

    second smoke

---
+++ DemoQATests/Smokes/RegisterTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewUITestFW.Controls;
+using NewUITestFW.DemoQAPages;
+using NUnit.Framework;
+
+namespace DemoQATests.Smokes
+{
+    class RegisterTest
+    {
+        [SetUp]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [Test]
+        public void NewUserCanBeRegistered()
+        {
+            new RegistrationPage();
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Close();
+        }
+    }
+}
---
+++ NewUITestFW/DemoQAPages/HomePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewUITestFW.Controls;
+using OpenQA.Selenium;
+
+namespace NewUITestFW.DemoQAPages
+{
+    class HomePage
+    {
+
+        public RegistrationPage OpenRegistrationPage()
+        {
+            var reg = Driver.Instance.FindElement(By.XPath(""));
+            reg.Click();
+            return new RegistrationPage();
+        }
+    }
+}
