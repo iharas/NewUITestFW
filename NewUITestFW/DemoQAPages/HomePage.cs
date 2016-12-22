9620058353bef5d9c78c2ad4e4982616b591d35b
vancho815 <vancho815@gmail.com>
2016-12-22 17:59:09 +0200

    sdsdsd

--- NewUITestFW/DemoQAPages/HomePage.cs
+++ NewUITestFW/DemoQAPages/HomePage.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NewUITestFW.Controls;
+﻿using NewUITestFW.Controls;
 using OpenQA.Selenium;
 
 namespace NewUITestFW.DemoQAPages
